52437e21afc25eb454ad37ef1db494a8519a1ce1
line437x <line437x@stud.kea.dk>
2021-09-15 12:29:46 +0200

    kommentarrer i JS

--- indexfilter.js
+++ indexfilter.js
@@ -2,29 +2,27 @@
 let topDestinationer;
 const container = document.querySelector(".data_container");
 const temp = document.querySelector("template");
+let filter = "ja";
+let dest;
 
 // Opret forbindelse til restdb herunder link og nøgle
 const url = "https://destinationer-f732.restdb.io/rest/destinationer";
 const options = {
   headers: { "x-apikey": "613b165043cedb6d1f97ef44" },
 };
-
+// Oprettelse af start function. denne starter når DOM'en er loaded
 document.addEventListener("DOMContentLoaded", start);
-let filter = "ja";
-let dest;
-
 function start() {
   hentdata();
 }
-
 // Her hentes data ind fra restdb via fetch
 async function hentdata() {
   const response = await fetch(url, options);
   topDestinationer = await response.json();
   visListe();
 }
-
-// Når menuen vises, bestemmers der her hvad der skal vises om hver destination og hvor informationerne om disse skal placeres henne
+// Når menuen vises, bestemmers der her hvad der skal vises -
+// om hver destination og hvor informationerne om disse skal placeres henne
 function visListe() {
   console.log(topDestinationer);
   topDestinationer.forEach((dest) => {
